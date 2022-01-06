def9dfd6a3310b09bf14c0b722288c40c47f56b7
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2022-01-06 09:29:25 +0200

    Added item to the UI list

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const ItemCtrl = (function () {
 
     const data = {
         items: [
-            {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 1, name: 'Cookie', calories: 400},
-            {id: 2, name: 'Eggs', calories: 300}
+            // {id: 0, name: 'Steak Dinner', calories: 1200},
+            // {id: 1, name: 'Cookie', calories: 400},
+            // {id: 2, name: 'Eggs', calories: 300}
         ],
         total: 0
     }
@@ -74,6 +74,21 @@ const ItemCtrl = (function () {
                 name:document.querySelector(UISelectors.itemNameInput).value,
                 calories:document.querySelector(UISelectors.itemCaloriesInput).value
             }
+        },
+        addListItem: function (item){
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.id = `item-${item.id}`;
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>`;
+            // insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend',li)
+        },
+        clearInput: function (){
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
         }
     }
 
@@ -91,7 +106,9 @@ const App = (function(ItemCtrl,  UICtrl ) {
         const input = UICtrl.getItemInput()
         if(input.name != '' && input.calories !== ''){
             const newItem = ItemCtrl.addItem(input.name, input.calories)
-            console.log(newItem)
+            UICtrl.addListItem(newItem)
+            // clear field
+            UICtrl.clearInput();
         }
         event.preventDefault()
     }
